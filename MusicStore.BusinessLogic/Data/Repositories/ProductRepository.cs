58aa990c301b64f65f1abcb368ac87775fdbd5d6
stefangalit21 <stefangalit1@gmail.com>
2025-04-16 12:51:44 +0300

    ProductRepository logic

--- MusicStore.BusinessLogic/Data/Repositories/ProductRepository.cs
+++ MusicStore.BusinessLogic/Data/Repositories/ProductRepository.cs
@@ -1,11 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MusicStore.Domain.Entities.Product;
 
-namespace MusicStore.BusinessLogic.Data.Repositories
+
+namespace MusicStore.BussinesLogic.Data.Repositories
 {
-	public class ProductRepository
-	{
-	}
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public override async Task<List<ProductData>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override async Task<ProductData> GetByIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override async Task AddAsync(ProductData entity)
+        {
+            _context.Products.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public override async Task UpdateAsync(ProductData entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override async Task DeleteAsync(ProductData entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override async Task<List<ProductData>> GetProductsByArtistAsync(int artistId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override async Task<List<ProductData>> GetProductsByGenreAsync(string genre)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override async Task<List<ProductData>> GetProductsByBpmAsync(int bpm)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override async Task<List<ProductData>> GetProductsByScaleAsync(string scale)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override async Task<List<ProductData>> GetProductsByProducerAsync(int producerId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
