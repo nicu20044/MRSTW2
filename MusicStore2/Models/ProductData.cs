66894bc825bcd37f7fad4598dbd53b229e48ed18
stefangalit21 <stefangalit1@gmail.com>
2025-04-16 12:31:31 +0300

    ProductViewModel adaugare

--- MusicStore2/Models/ProductData.cs
+++ MusicStore2/Models/ProductData.cs
@@ -1,7 +1,20 @@
-﻿namespace MusicStore2.Models
+﻿using System.Web;
+
+namespace MusicStore.web.Models
 {
-    public class ProductData
+    public class ProductViewModel
     {
-        
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public int Bpm { get; set; }
+        public string Scale { get; set; }
+        public string License { get; set; }
+        public string ArtistName { get; set; }
+        public int ArtistId { get; set; }
+        public int ProducerId { get; set; }
+        public HttpPostedFileBase ImageFile { get; set; }
+        public HttpPostedFileBase AudioFile { get; set; }
     }
 }
