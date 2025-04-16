35719b5221b862fbdd0f8add00db8966c7ee3b98
stefangalit21 <stefangalit1@gmail.com>
2025-04-16 12:42:25 +0300

    Add logic for userservice

--- MusicStore.BusinessLogic/Services/UserService.cs
+++ MusicStore.BusinessLogic/Services/UserService.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities.User;
+using MusicStore.BusinessLogic.Data;
+using MusicStore.Domain.Entities;
+using MusicStore.BussinesLogic.Data;
 
-namespace MusicStore.BusinessLogic.Services
+namespace BusinessLogic.Services
 {
-	public class UserService
-	{
-	}
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+    
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+    
+        public async Task<AppUser> GetUserByIdAsync(int userId) => await _userRepository.GetByIdAsync(userId);
+        public async Task RegisterUserAsync(AppUser user) => await _userRepository.AddAsync(user);
+    }
 }
