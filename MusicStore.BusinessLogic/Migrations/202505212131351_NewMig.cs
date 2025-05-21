6ad91db03a9597990e792baad0fd1adc917f03df
stefangalit21 <stefangalit1@gmail.com>
2025-05-22 00:35:55 +0300

    adding new migratiom

---
+++ MusicStore.BusinessLogic/Migrations/202505212131351_NewMig.cs
@@ -0,0 +1,35 @@
+﻿namespace MusicStore.BusinessLogic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NewMig : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.UserCartItems",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.ProductDatas", t => t.ProductId, cascadeDelete: true)
+                .ForeignKey("dbo.AppUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.ProductId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.UserCartItems", "UserId", "dbo.AppUsers");
+            DropForeignKey("dbo.UserCartItems", "ProductId", "dbo.ProductDatas");
+            DropIndex("dbo.UserCartItems", new[] { "ProductId" });
+            DropIndex("dbo.UserCartItems", new[] { "UserId" });
+            DropTable("dbo.UserCartItems");
+        }
+    }
+}
